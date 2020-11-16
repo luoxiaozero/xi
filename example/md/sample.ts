1c744fe2fe470c20721c50074982129b9722179d
liziqiang <liziqiang01@163.com>
2020-11-16 20:18:05 +0800

    fix(md): sample文件的***不转义

--- example/md/sample.ts
+++ example/md/sample.ts
@@ -58,7 +58,7 @@ Markdown has a series of derivative versions to extend the functions of Markdown
 
 #### 图片
 
-![ad](imgs/tu--example.jpg)
+![ad](imgs/tu--imgs.jpg)
 
 #### 无序列表
 
@@ -144,4 +144,10 @@ class bb:
 
 let lines = new String(md);  
 lines = lines.substring(lines.indexOf("/*") + 3, lines.lastIndexOf("*/"));  
+/**删除每行的最后一个字符(\n) */
+let arr = lines.split('\n')
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].substring(0, arr[i].length - 1)
+}
+lines = arr.join('\n');
 export default lines;
