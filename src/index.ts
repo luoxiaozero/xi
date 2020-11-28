aadc9f8da529ed598f11a87fa37469021c4bf411
玉意 <liziqiang01@163.com>
2020-11-28 16:14:13 +0800

    refactor(ArtText): ArtText重构

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import ArtText from '@/artText'
-import Core from '@/core'
+import { Core } from '@/core'
 
-
-const win: any = window;
-win.ArtText = ArtText; // 挂载到window上
-Core.loaded();
+Core.use(ArtText);
 export default ArtText;
