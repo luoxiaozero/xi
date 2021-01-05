0343a129475f261c127fca4a318773e295a5cda1
玉意 <liziqiang01@163.com>
2021-01-05 16:06:06 +0800

    🐞fix(Sidebar): menu的点击后显示main

--- src/plugins/sidebar/index.ts
+++ src/plugins/sidebar/index.ts
@@ -7,8 +7,10 @@ export default class Sidebar {
     private art: Art;
     dom: HTMLDivElement;
     menuDom: HTMLDivElement;
+    selectMenuDom: HTMLSpanElement;
     mainDom: HTMLDivElement;
     openOrCloseFlag: boolean;
+    info: Map<string, HTMLElement>;
     constructor(art: Art) {
         this.art = art;
         this.dom = document.createElement('div');
@@ -27,10 +29,14 @@ export default class Sidebar {
         this.mainDom.setAttribute('class', 'art-sidebar-main');
         this.dom.appendChild(this.mainDom);
 
+        this.selectMenuDom = null;
+        this.info = new Map();
+
         this.art.get<Tool>('$tool').add([{ dom: this.dom, place: 'Editor.before' }]);
     }
 
     public add(menuText: string, mainDom: HTMLElement) {
+        this.info.set(menuText, mainDom);
         let span = document.createElement('span');
         span.setAttribute('class', 'art-sidebar-menu-span')
         span.innerHTML = menuText;
@@ -40,7 +46,27 @@ export default class Sidebar {
         menuDomChild.setAttribute('style', 'flex: 1');
         this.menuDom.appendChild(menuDomChild);
 
+        mainDom.style.display = 'none';
         this.mainDom.appendChild(mainDom);
+
+        const _this = this;
+        span.onclick = function()  {
+            _this.openOption(this as HTMLSpanElement);
+        }
+
+        if(!this.selectMenuDom)
+            span.click()
+    }
+
+    public openOption(span: HTMLSpanElement) {
+        if (this.selectMenuDom) {
+            this.selectMenuDom.classList.remove('art-sidebar-menu-select');
+            this.info.get(this.selectMenuDom.innerHTML).style.display = 'none';
+        }
+        this.selectMenuDom = span;
+
+        span.classList.add('art-sidebar-menu-select');
+        this.info.get(this.selectMenuDom.innerHTML).style.display = 'inherit';
     }
 
     public openOrClose(): void {
@@ -58,7 +84,8 @@ export let SidebarExport = {
         Core.use(openSidebar);
         options['container'].bind('sidebar', Sidebar, [{'get': 'art'}], true);
         options['Tool'].addCss('.art-sidebar{width:300px;margin-left: -333px;top: 20px;bottom: 20px;position: fixed;font-size:13.5px;background-color: #fff;box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);border-radius: 3px;color: #666;} \
-                                .art-sidebar-menu{height: 28px;display: flex;border-bottom: 1px #eee solid;font-weight: 600;padding: 10px 20px 6px;text-align:center;font-size: 14px}.art-sidebar-menu-span{border-bottom: 4px #666 solid;padding: 0 15px 5px;cursor: pointer;}\
+                                .art-sidebar-menu{height: 28px;display: flex;border-bottom: 1px #eee solid;font-weight: 600;padding: 10px 20px 6px;text-align:center;font-size: 14px}.art-sidebar-menu-span{padding: 0 15px 5px;cursor: pointer;}\
+                                .art-sidebar-menu-select{border-bottom: 4px #666 solid;}\
                                 .art-sidebar-main{height: calc(100% - 50px);padding: 3px 0 4px}')
     },
     created: (art: Art , options) => art.get<Sidebar>('sidebar')
