cf28ef126875455ffa63b23564bdf89b8df6143d
玉意 <liziqiang01@163.com>
2021-01-07 19:54:54 +0800

    ✨feat(FileSidebar): 打开文件和文件夹

--- plugins/fileSidebar/index.ts
+++ plugins/fileSidebar/index.ts
@@ -1,11 +1,15 @@
+import Editor from '@/editor';
 import { Art } from '../../src/core';
 import Sidebar from '../../src/plugins/sidebar';
+import ArtRequest from '../artRequest';
 
 class FileSidebar {
     dom: HTMLDivElement;
     art: Art;
     file: any;
     openFolderDom: HTMLDivElement;
+    folderDom: HTMLUListElement;
+    selectDom: HTMLSpanElement;
     constructor(art: Art) {
         this.dom = document.createElement('div');
         this.dom.setAttribute('class', 'art-fileSidebar')
@@ -16,30 +20,57 @@ class FileSidebar {
         this.openFolderDom = document.createElement('div');
         this.openFolderDom.setAttribute('class', 'art-fileSidebar-openFolder')
         this.openFolderDom.innerHTML = '打开文件夹';
+        this.openFolderDom.onclick = () => this.openFolder();
         this.dom.appendChild(this.openFolderDom);
-        this.updata();
+        
+        this.folderDom = document.createElement('ul');
+        this.dom.appendChild(this.folderDom);
+    }
+
+    private openFolder() {
+        const _this = this;
+        this.art.get<ArtRequest>('artRequest').get('/openFolder').then((json: any)=> {
+            _this.file = json;
+            _this.updata();
+        })
     }
 
     public updata() {
-        let ul = document.createElement('ul');
-        ul.appendChild(this.openFile(this.file));
-        this.dom.appendChild(ul);
+        this.folderDom.innerHTML = '';
+        let li = this.openFile(this.file);
+        this.selectDom = li.childNodes[0] as HTMLSpanElement;
+        this.selectDom.classList.add('art-fileSidebar-select');
+        this.folderDom.appendChild(li);
     }
 
     public openFile(file: any): HTMLLIElement {
+        const _this = this;
+
         let li = document.createElement('li');
         let span = document.createElement('span');
+        span.setAttribute('art-path', file.path)
         span.innerHTML = file.name;
         li.appendChild(span);
 
         if (file.type == '') {
             let ul = document.createElement('ul');
-            for (let f of file.files) {
-                ul.appendChild(this.openFile(f));
-            }
+            if (file.files)
+                for (let f of file.files) 
+                    ul.appendChild(this.openFile(f));
+            
             li.appendChild(ul);
         } else {
             li.setAttribute('class', 'art-fileSidebar-file')
+            
+            span.onclick = function() {
+                _this.selectDom.classList.remove('art-fileSidebar-select');
+                _this.selectDom = this as HTMLSpanElement;
+                _this.selectDom.classList.add('art-fileSidebar-select');
+                let path = _this.selectDom.getAttribute('art-path');
+                _this.art.get<ArtRequest>('artRequest').get('/openFileText?path=' + path).then((json: any)=> {
+                    _this.art.get<Editor>('$editor').openFile(json);
+                })
+            }
         }
         return li;
     }
@@ -51,7 +82,7 @@ export let FileSidebarExport = {
         options['Tool'].addCss('.art-fileSidebar-file{list-style: none;margin-left: -18px;}.art-fileSidebar{padding: 0 10px;height: 100%;overflow: auto;}.art-fileSidebar-openFolder{text-align: center;margin: 1em 30px 0.6em;padding: 6px;cursor: pointer;background: #1abc9c;color: #fff;font-weight: 600;}.art-fileSidebar-openFolder:hover{background: #1abc9ce0;}\
         .art-fileSidebar ul{margin: 0; padding-left: 18px}.art-fileSidebar li{margin-top: 5px;}.art-fileSidebar span{width: calc(100% - 6px);;display: inline-block;cursor: pointer;padding: 2px 0 2px 6px}.art-fileSidebar span:hover{background-color: #eee}\
         .art-fileSidebar::-webkit-scrollbar {width: 10px;height:0}\
-            .art-fileSidebar::-webkit-scrollbar-thumb {background: #ccc;}')
+            .art-fileSidebar::-webkit-scrollbar-thumb {background: #ccc;}.art-fileSidebar-select{font-weight: bolder;}')
     },
     created: (art: Art) => {
         art.get<Sidebar>('sidebar').add('文件', art.get<FileSidebar>('fileSidebar').dom);
