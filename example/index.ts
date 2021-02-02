2d0d8bed34de953d92f834b945b5b6361876bf07
玉意 <liziqiang01@163.com>
2021-02-02 17:56:25 +0800

    📃docs: 添加table文本

--- example/index.ts
+++ example/index.ts
@@ -1,48 +1,11 @@
 import ArtText from '../src'
 import './imgs/tu.jpg'
 import sample from './md/sample'
-'\n\
-1. 无序列表3\n\
-   2. 无序列表4\n\
-   3. 无序列表5\n\
-\n\
-1234\n\
-'
+import table from './md/table'
 
+let defaultMd = table;
 
-'1. 无序列表3\n\
-  * 无序列表4\n\
-  * 无序列表5\n\
-    * 无序列表6\n\
-    * * 无序列表6\n\
-'
-
-'1. 无序列表1\n\
-2. 1. 给岁月以文明，而不是给文明以岁月。2\n\
-   2. 无序列表3\n\
-'
-
-'1. 有序列表1\n\
-2. 有序列表2\n\
-   * 无序列表\n\
-   * 无序列表2\n\
-3. 有序列表3\n\
-'
-let md =
-'\n\
-* [x] 已完成\n\
-* [ ] 未完成\n\
-#### 引用\n\
-* 已完成\n\
-* 未完成\n\
-\n\
-* 已完成\n\
-* 未完成\n\
-* * dasdd\n\
-  * \n\
-'
 window.onload = function () {
-   console.log(sample);
-    const art: ArtText = new ArtText({ defaultMd: sample }).mount('#art');
+    const art: ArtText = new ArtText({ defaultMd: defaultMd }).mount('#art');
     window['art'] = art;
 }
